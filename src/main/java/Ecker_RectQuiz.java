df38b93b5f5ce0eaea08d79f683c1ad3e0fb670d
QuirinEcker <quirin.ecker@bajupa.com>
2018-05-15 11:13:33 +0200

    finishing project

--- src/main/java/Ecker_RectQuiz.java
+++ src/main/java/Ecker_RectQuiz.java
@@ -1,20 +1,23 @@
 import processing.core.PApplet;
 
 public class Ecker_RectQuiz extends PApplet {
-
     public static void main(String[] args) {
-        PApplet.main("Ecker_RectQuiz");
+        PApplet.main("Ecker_RectQuiz", args);
     }
 
     public void settings() {
-        size(500,500);
+        size(800, 800);
     }
 
     public void setup() {
+        frameRate(120);
         background(255,255,255,255);
     }
-
     public void draw() {
-        rect(250,250, mouseX, mouseX );
+        int x = (width - mouseX)/2;
+        int y = (width - mouseX)/2;
+
+        background(255,255,255,255);
+        rect(x, y, mouseX, mouseX);
     }
 }
